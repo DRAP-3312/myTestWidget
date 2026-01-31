ecf9beda58f9033b396af7eeb265fcc47fe4daa8
DRAP-3312 <davidake117@gmail.com>
2026-01-30 20:21:28 -0500

    feat: dependency pinia added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 import "vue-chat-widget-drap";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(createPinia());
+app.mount("#app");
